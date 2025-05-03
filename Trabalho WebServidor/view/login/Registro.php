f7b1dcce3e876eb4ccd545f515e2356d3a9acf01
idkAndrad3 <eduardomassuandrade@gmail.com>
2025-05-03 08:47:31 -0300

    Boostrap Registro 2.php

--- Trabalho WebServidor/view/login/Registro.php
+++ Trabalho WebServidor/view/login/Registro.php
@@ -33,9 +33,7 @@
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.5/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-SgOJa3DmI69IUzQ2PVdRZhwQ+dy64/BUtbMJw1MZ8t5HZApcHrRKUc4W0kG879m7" crossorigin="anonymous">
     <style>
         body {
-            font-family: 'Arial', sans-serif;
             background-color: #121212;
-            color: #ffffff;
             display: flex;
             justify-content: center;
             align-items: center;
@@ -83,21 +81,6 @@
         .form-group input:focus {
             outline: none;
             box-shadow: 0 0 5px #00ff00;
-        }
-
-        .register-button {
-            background-color: #00ff00;
-            color: #121212;
-            border: none;
-            padding: 10px 20px;
-            border-radius: 5px;
-            font-size: 16px;
-            cursor: pointer;
-            width: 100%;
-        }
-
-        .register-button:hover {
-            background-color: #028202;
         }
 
         .back-icon img {
