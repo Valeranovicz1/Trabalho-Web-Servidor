76eadadb2681c25756fb4b7e8904ebb29eedc75d
idkAndrad3 <eduardomassuandrade@gmail.com>
2025-05-21 20:08:24 -0300

    Bootstrap painelEmpresa

--- Trabalho WebServidor/view/painel/painelEmpresa.php
+++ Trabalho WebServidor/view/painel/painelEmpresa.php
@@ -39,7 +39,7 @@
     <title>ET Games</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.5/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-SgOJa3DmI69IUzQ2PVdRZhwQ+dy64/BUtbMJw1MZ8t5HZApcHrRKUc4W0kG879m7" crossorigin="anonymous">
     <link rel="stylesheet" href="../../assets/css/style-painel.css">
-    <link rel="icon" href="img/logo/logo.png" type="image/png">
+    <link rel="icon" href="../../assets/img/logo/logo.png" type="image/png">
     
 </head>
 <body>
